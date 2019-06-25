6180541f4be8b854a96244a6b98690e0fcc912fa
XO <xiangbolong@gmail.com>
2019-06-25 17:29:18 +0800

    调整新的

--- TZImagePickerController/TZImagePickerController/NSBundle+ZLPhotoBrowser.h
+++ TZImagePickerController/TZImagePickerController/NSBundle+ZLPhotoBrowser.h
@@ -1,21 +1,21 @@
+////
+////  NSBundle+ZLPhotoBrowser.h
+////  ZLPhotoBrowser
+////
+////  Created by long on 16/10/20.
+////  Copyright © 2016年 long. All rights reserved.
+////
 //
-//  NSBundle+ZLPhotoBrowser.h
-//  ZLPhotoBrowser
+//#import <Foundation/Foundation.h>
 //
-//  Created by long on 16/10/20.
-//  Copyright © 2016年 long. All rights reserved.
+//@interface NSBundle (ZLPhotoBrowser)
 //
-
-#import <Foundation/Foundation.h>
-
-@interface NSBundle (ZLPhotoBrowser)
-
-+ (instancetype)zlPhotoBrowserBundle;
-
-+ (void)resetLanguage;
-
-+ (NSString *)zlLocalizedStringForKey:(NSString *)key;
-
-+ (NSString *)zlLocalizedStringForKey:(NSString *)key value:(NSString *)value;
-
-@end
+//+ (instancetype)zlPhotoBrowserBundle;
+//
+//+ (void)resetLanguage;
+//
+//+ (NSString *)zlLocalizedStringForKey:(NSString *)key;
+//
+//+ (NSString *)zlLocalizedStringForKey:(NSString *)key value:(NSString *)value;
+//
+//@end
